6de68b809b5a7f387681e4201b43f7eca7736e5c
VIRUS <56464574+krishna-kenny4@users.noreply.github.com>
2020-06-21 18:19:35 +0530

    Add files via upload

--- search_algorithm.py
+++ search_algorithm.py
@@ -4,11 +4,13 @@ def search_algorithm(text, word):
 
     text = " "+text
     stage_list = []
+    remember_index = []
 
     while text_letter_counter < len(text):
 
         if text[text_letter_counter] == word[word_letter_counter]:
             char_lst = []
+            remember_index.append(text_letter_counter)
 
             for z in range(len(word)):
                 char_lst.append(text[text_letter_counter])
@@ -20,7 +22,16 @@ def search_algorithm(text, word):
         else:
             text_letter_counter += 1
 
-    return stage_list
+    index = 0
+    position_list = []
+    for string_ in stage_list:
+        if string_ == list(word):
+            position_list.append(remember_index[index]-1)
+        index += 1
+
+    return position_list
 
 
-print(search_algorithm("i am awesome", "awesome"))
+text = input("Enter text: ")
+word = input("Enter word: ")
+print(search_algorithm(text, word))
